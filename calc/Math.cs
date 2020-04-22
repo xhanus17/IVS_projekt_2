3df75a50c61733fe3fcaffecc99f9ebc3068d197
xhanus17 <63417762+xhanus17@users.noreply.github.com>
2020-04-22 02:35:31 +0200

    math renamed, changed Div, Factorial adde
    changed class name to CalcMath because it could get mistaken for built-in function Math, made changes to div function, now div is just division of two numbers and tryDiv is more sofysticated with exception handling + added new function for integer factorial calculation

--- calc/Math.cs
+++ calc/Math.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 namespace MathNS
 {
-    public class math
+    public static class CalcMath
     {
 
         public static double Sum(double cis1, double cis2) => cis1 + cis2;
@@ -14,23 +14,40 @@ namespace MathNS
         public static double Mul(double cis1, double cis2) => cis1 * cis2;
         public static double Div(double cis1, double cis2)
         {
+            return cis1 / cis2;
+        }
+        public static bool TryDiv(double cis1, double cis2, out double vysledek)
+        {
             try
             {
-                if (cis2 == 0)
-                {
-                    throw new DivideByZeroException();
-                }
-                return cis1 / cis2;
+                vysledek = Div(cis1, cis2);
+                return true;
             }
-            catch (DivideByZeroException){ return 0; }
+            catch
+            {
+                vysledek = 0;
+                return false;
+            }
         }
         public static double Pow(double cis1, double cis2)
         {
             return Math.Pow(cis1, cis2);
         }
         public static double Sqrt(double cis1,double cis2) {//Odmocnina 2
-            double val = Div(1, cis1);
-            return math.Pow(cis2, 1 / cis1);
+            double val = 0;
+            if (TryDiv(1, cis1, out val))
+            {
+                return Math.Pow(cis2, val);
+            }
+            else return 0;
+        }
+        public static double Factorial(int num_base)
+        {
+            for(int i = num_base-1;i>1;i--)
+            {
+                num_base *= i;
+            }
+            return num_base;
         }
     }
 }
